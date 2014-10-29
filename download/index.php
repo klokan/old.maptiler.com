2cfbf5804f56f9e8c2e6d4583ebce88635f0829b
dala <dalibor.janak@gmail.com>
2014-10-29 11:51:54 +0100

    Download via hash

--- download/index.php
+++ download/index.php
@@ -1,28 +1,29 @@
 <?php
 
-if (isset($_GET['url']) && !empty($_GET['url']) && isset($_GET['email']) && isset($_GET['key'])) {
-  $product = 'pro';
-  $url = $_GET['url'];
-  $email = $_GET['email'];
-  $key = $_GET['key'];
-
+if (isset($_GET['url']) && !empty($_GET['url'])) {
   //libs form pricing
   include_once '../pricing/limeLM.php';
   include_once '../pricing/libs/S3.php';
   include_once '../pricing/secure.php';
-
-  //auth
   $LM = new Maptiler\Limelm;
-  if ($LM->verifyUser($email, $key)) {
+
+  $email = $_GET['email'];
+  $key = $_GET['key'];
+
+  if (isset($_GET['hash']) && $_GET['hash'] == md5($keys['awsAccessKey']) || $LM->verifyUser($email, $key)) {
+
+    $url = $_GET['url'];
+
+    //auth
     $s3 = @new S3($keys['awsAccessKey'], $keys['awsSecretKey']);
     $DownloadLink = @S3::getAuthenticatedURL('downloads.klokantech.com'
                     , 'maptiler/' . $url, 60 * 60 * 24 * 5, true);
     header('location: ' . $DownloadLink);
   } else {
-    echo 'Verification failed.';
+    echo 'Verification failed!';
+    die;
   }
 } else {
-
   include 'config.php';
 
   $userAgent = $_SERVER['HTTP_USER_AGENT'];
