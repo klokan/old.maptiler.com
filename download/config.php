20019f0d42d3f935ae454debb63515066a7c95a0
dala <dalibor.janak@gmail.com>
2014-10-29 10:04:09 +0100

    Download linux

--- download/config.php
+++ download/config.php
@@ -1,5 +1,10 @@
 <?php
 
 $urlDemoWin = 'http://downloads.klokantech.com/maptiler/maptiler-0.5.4-free-x64-x86-setup.exe';
-$urlDemoLinux = 'http://downloads.klokantech.com/maptiler/maptiler-0.5.4-free-x64-x86-setup.exe';
+
+$urlDemoLinux['Debian'] = array('dist' => 'Debian', 'title' => '7 Wheezy', 'link' => 'http://downloads.klokantech.com/maptiler/maptiler-0.5.5-free-linux.debian-wheezy.amd64.deb');
+$urlDemoLinux['Ubuntu'] = array('dist' => 'Ubuntu','title' => 'Trusty', 'link' => 'http://downloads.klokantech.com/maptiler/maptiler-0.5.5-free-linux.ubuntu-trusty.amd64.deb');
+$urlDemoLinux['Redhat'] = array('dist' => 'Redhat','title' => 'RHEL 7 *', 'link' => 'http://downloads.klokantech.com/maptiler/maptiler-0.5.5-free-linux.el7.x86_64.rpm');
+$urlDemoLinux['Fedora'] = array('dist' => 'Fedora','title' => 'Fedora 20 *', 'link' => 'http://downloads.klokantech.com/maptiler/maptiler-0.5.5-free-linux.fc20.x86_64.rpm');
+
 $urlDemoMac = 'http://downloads.klokantech.com/maptiler/maptiler-0.5.4-free-mac.dmg';
